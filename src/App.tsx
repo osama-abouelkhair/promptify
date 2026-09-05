d4600cb000186c8c5be702c425adf2103f917249
osama-abulkhair <osama.abulkhair@gmail.com>
2026-09-05 23:47:44 +0200

    improve side bar names

--- src/App.tsx
+++ src/App.tsx
@@ -201,7 +201,7 @@ function App() {
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">
                   <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
                 </svg>
-                {prompt.name}
+                {prompt.name.replace(/-/g, ' ')}
               </Link>
             )
           })}
